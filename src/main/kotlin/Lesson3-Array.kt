296df72accac75f749045e19e241630b8eb82ba3
Ciel <me@cieldev.net>
2022-08-15 01:37:29 +0900

    ｄｓ

---
+++ src/main/kotlin/Lesson3-Array.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val list1 = arrayListOf("Sato", "Suzuki",  "Tanaka", "Honda")
+    println(list1[0])
+    println(list1[1])
+    println(list1[2])
+    println(list1[3])
+}
+
+/*
+文字列などを取り出し可能な箱に格納する変数
+arrayListOf以外も存在する
+
+インデックスは前から0, 1, 2, 3...となる
+
+複数の配列を統合可能
+一層の配列を1次元配列
+二層の配列を2次元配列
+三層の配列を3次元配列
+などとよぶ
+
+以下2次元配列の例
+ */
